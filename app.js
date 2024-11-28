338b11c237eff62c22de50377d9b8b451b2a75b8
LiniiS <enila404@gmail.com>
2024-11-28 00:59:31 -0300

    feature: moving to pwa

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Carregar dados das vagas
     fetch('jobs.json')
         .then(response => response.json())
         .then(data => {
@@ -18,8 +19,51 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         })
         .catch(error => console.error('Erro ao carregar os dados das vagas:', error));
+
+    // Registrar o Service Worker
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/service-worker.js')
+                .then((registration) => {
+                    console.log('Service Worker registrado com sucesso:', registration);
+                })
+                .catch((error) => {
+                    console.log('Erro ao registrar o Service Worker:', error);
+                });
+        });
+    }
+
+    // Gerenciar o evento "beforeinstallprompt" para permitir a instalação do PWA
+    let deferredPrompt;
+    const installButton = document.getElementById('install-button');
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+        // Prevenir o comportamento padrão do navegador
+        event.preventDefault();
+        // Armazenar o evento para ser disparado quando o usuário clicar no botão de instalação
+        deferredPrompt = event;
+
+        // Exibir o botão de "Adicionar à Tela Inicial"
+        if (installButton) {
+            installButton.style.display = 'block';
+        }
+
+        // Ao clicar no botão de instalação
+        installButton.addEventListener('click', () => {
+            // Mostrar o prompt de instalação
+            deferredPrompt.prompt();
+
+            // Esperar pela escolha do usuário
+            deferredPrompt.userChoice.then((choiceResult) => {
+                console.log(choiceResult.outcome);
+                deferredPrompt = null;  // Limpar o evento após o prompt
+            });
+        });
+    });
+
 });
 
+// Função para abrir o modal com detalhes da vaga
 function openModal(id) {
     fetch('jobs.json')
         .then(response => response.json())
@@ -39,6 +83,7 @@ function openModal(id) {
         .catch(error => console.error('Erro ao carregar os detalhes da vaga:', error));
 }
 
+// Função para fechar o modal
 function closeModal() {
     document.getElementById('modal').style.display = 'none';
 }
